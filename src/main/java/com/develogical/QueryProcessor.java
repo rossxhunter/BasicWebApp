65c61daf128b69d6f1280dae6e4219f0c135906d
Ross Hunter <rossxhunter@gmail.com>
2020-01-17 11:02:43 +0000

    Edit QueryProcessor

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,6 +8,9 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        else if (query.toLowerCase().contains("imperial")) {
+            return "Imperial is a university";
+        }
         return "";
     }
 }
--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -24,4 +24,9 @@ public class QueryProcessorTest {
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
+
+    @Test
+    public void knowsAboutImperial() throws Exception {
+        assertThat(queryProcessor.process("Imperial"), containsString("university"));
+    }
 }
